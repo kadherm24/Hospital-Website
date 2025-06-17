f317088fd0aab86d5dc1302d25754deb9f401037
kadherm24 <kadherm24@gmail.com>
2025-06-17 18:14:59 +0530

    patient CRUD operations

--- hospital/authentication/urls.py
+++ hospital/authentication/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='signup'),
     
     
-    path('addpatient/', views.AddPatient.as_view(), name='addpatient')
+    path('addpatient/', views.AddPatient.as_view(), name='addpatient'),
+    path('patientview/', views.patientView, name='patientview'),
+    path('patient/<int:pk>', views.PatientDetails.as_view(), name = 'patient_detail'),
+    path('patient/edit/<int:pk>', views.EditPatient.as_view(), name = 'edit_patient'),
+    path('patient/del/<int:pk>', views.DeletePatient.as_view(), name = 'del_patient'),
 ]
--- hospital/authentication/views.py
+++ hospital/authentication/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LoginView
 from .models import Patient
 from django.urls import reverse_lazy # finds the url path associated with a url name
 
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView,  DetailView
 
 from .forms import CustomLoginForm, CustomRegistrationForm, AddPatientForm
 
@@ -24,6 +24,8 @@ class UserRegisterView(CreateView):
     template_name = 'signup.html'
     success_url = reverse_lazy('signin')# redirects user to login page after registeration
 
+
+
 #CRUD operations for Patient
 #1. Create View
 class AddPatient(CreateView):
@@ -36,5 +38,37 @@ class AddPatient(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user 
         return super().form_valid(form)
+    
+#2.Read
+#ListView
+def patientView(request):
+    patient = Patient.objects.filter(user = request.user) # SELECT * FROM patient;
+    context = {
+        'patients' : patient
+
+    }
+    template = 'patients.html'
+    return render(request, template, context)
+
+#Detailview
+class PatientDetails(DetailView):
+    model = Patient
+    template_name = 'patient_details.html'
+    context_object_name = 'patient'
+    success_url = '/'
+
+
+class EditPatient(UpdateView):
+    model = Patient
+    fields = '__all__'
+    template_name = 'edit_patient.html'
+    success_url = '/'
+
+# 4. Delete view (Delete statement)
+class DeletePatient(DeleteView):
+    model = Patient
+    template_name = 'delete_patient.html'
+    success_url = '/'
+
     
     
